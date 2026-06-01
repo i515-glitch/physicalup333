90752a9f2d725d7a7f1bb6e32718d236782c93f9
i515-glitch <hellomidas@gmail.com>
2026-06-01 15:31:02 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,4 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'app.html'
-    }
-  }
 })
